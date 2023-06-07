6372bc1594a6b4dd5cc98886e0c627b790b44b4e
Alexandr Gaponenko <sag5223@gmail.com>
2023-06-07 21:26:09 +0700

    Добавьте файлы проекта.

---
+++ wpf/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+namespace wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int speedx = 0;
+        private int speedy = 0;
+        private int speedxnap = 1;
+        private int speedynap = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer dispatcherTimer1 = new DispatcherTimer();
+            dispatcherTimer1.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer1.Interval = new TimeSpan(0, 0, 0, 0, 20);
+            dispatcherTimer1.Start();
+
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            Canvas.SetLeft(image, Canvas.GetLeft(image) - (speedx * speedxnap));
+            Canvas.SetTop(image, Canvas.GetTop(image) - (speedy * speedynap));
+            if (((Canvas.GetLeft(image) < 5) || (Canvas.GetLeft(image) > 680)))
+            {
+                speedxnap = speedxnap * -1;
+                return;
+            }
+            if (((Canvas.GetTop(image) < 10) || (Canvas.GetTop(image) > 370)))
+            {
+                speedynap = speedynap * -1;
+                return;
+            }
+            if ((speedxnap == -1) && (speedynap == 1))
+            {
+                image.Source = BitmapFrame.Create(new Uri(@"D:\wpf\направовверх.png"));
+                return;
+            }
+            if ((speedxnap == -1) && (speedynap == -1))
+            {
+                image.Source = BitmapFrame.Create(new Uri(@"D:\wpf\направовниз.png"));
+                return;
+            }
+            if ((speedxnap == 1) && (speedynap == -1))
+            {
+                image.Source = BitmapFrame.Create(new Uri(@"D:\wpf\налевовниз.png"));
+                return;
+            }
+
+            if ((speedxnap == 1) && (speedynap == 1))
+            {
+                image.Source = BitmapFrame.Create(new Uri(@"D:\wpf\налевовверх.png"));
+                return;
+            }
+        }
+        private void UserControl1_RedEvent()
+        {
+            speedy = 0;
+            speedx = 0;
+
+        }
+        private void UserControl1_YellowEvent()
+        {
+            speedy = 3;
+            speedx = 3;
+
+        }
+        private void UserControl1_GreenEvent()
+        {
+            speedy = 8;
+            speedx = 8;
+        }
+    }
+}
---
+++ wpf/svetofor.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace wpf
+{
+    
+    public partial class svetofor : UserControl
+    {
+        public delegate void myHandler();
+        public event myHandler RedEvent;
+        public event myHandler GreenEvent;
+        public event myHandler YellowEvent;
+        string reg;
+
+
+        public svetofor()
+        {
+            InitializeComponent();
+            green.Fill = Brushes.Gray;
+            red.Fill = Brushes.Gray;
+            yellow.Fill = Brushes.Gray;
+            reg = "Red";
+
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+
+            if (reg == "Red")
+            {
+                red.Fill = Brushes.Red;
+                yellow.Fill = Brushes.Gray;
+                green.Fill = Brushes.Gray;
+                RedEvent?.Invoke();
+                reg = "Yellow";
+            }
+            else if (reg == "Yellow")
+            {
+                red.Fill = Brushes.Gray;
+                green.Fill = Brushes.Gray;
+                yellow.Fill = Brushes.Yellow;
+                YellowEvent?.Invoke();
+                reg = "Green";
+            }
+            else if (reg == "Green")
+            {
+                red.Fill = Brushes.Gray;
+                green.Fill = Brushes.Green;
+                yellow.Fill = Brushes.Gray;
+                GreenEvent?.Invoke();
+                reg = "Yellow2";
+
+            }
+            else if (reg == "Yellow2")
+            {
+                red.Fill = Brushes.Gray;
+                green.Fill = Brushes.Gray;
+                yellow.Fill = Brushes.Yellow;
+                YellowEvent?.Invoke();
+                reg = "Red";
+            }
+        }
+    
+
+
+    }
+}
